822d5af4377e688ebff441e71cb5ed32d886ec7d
Hong <gtv7854@naver.com>
2021-09-22 15:59:37 +0900

    Weekly_Challenge start

---
+++ src/Programmers/Weekly_Challenge/Recommend_A_Profession.java
@@ -0,0 +1,17 @@
+package Programmers.Weekly_Challenge;
+
+/*
+위클리 챌린지 > 1주차_부족한 금액 계산하기
+https://programmers.co.kr/learn/courses/30/lessons/82612
+
+제한 사항
+- 2 ≤ scores의 행의 길이(학생 수) ≤ 10
+- scores의 열의 길이 = scores의 행의 길이 즉, scores는 행과 열의 길이가 같은 2차원 배열입니다.
+- 0 ≤ scores의 원소 ≤ 100
+*/
+
+public class Recommend_A_Profession {
+    public static void main(String[] args) {
+
+    }
+}
